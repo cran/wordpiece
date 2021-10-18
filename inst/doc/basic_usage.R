1c22d2929cfa82219e0eac5b8daf9002f5287819
Jonathan Bratt <jonathan.bratt@macmillan.com>
2021-10-18 17:40:02 +0000

    version 2.0.1

--- inst/doc/basic_usage.R
+++ inst/doc/basic_usage.R
@@ -4,13 +4,26 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ----example0-----------------------------------------------------------------
+## ----default-vocabs-----------------------------------------------------------
 library(wordpiece)
+
+# The default vocabulary is uncased.
+wordpiece_tokenize(
+  "I like tacos!"
+)
+
+# A cased vocabulary is also provided.
+wordpiece_tokenize(
+  "I like tacos!",
+  vocab = wordpiece_vocab(cased = TRUE)
+)
+
+## ----example0-----------------------------------------------------------------
 # Get path to sample vocabulary included with package.
 vocab_path <- system.file("extdata", "tiny_vocab.txt", package = "wordpiece")
 
 # Load the vocabulary.
-vocab <- load_or_retrieve_vocab(vocab_path, use_cache = FALSE)
+vocab <- load_vocab(vocab_path)
 
 # Take a peek at the vocabulary.
 head(vocab)
@@ -26,7 +39,7 @@ attr(vocab, "is_cased")
 # Here is the same vocabulary, but containing the capitalized token "Hi".
 vocab_path2 <- system.file("extdata", "tiny_vocab_cased.txt", 
                            package = "wordpiece")
-vocab_cased <- load_or_retrieve_vocab(vocab_path2, use_cache = FALSE)
+vocab_cased <- load_vocab(vocab_path2)
 head(vocab_cased)
 
 # vocab_cased is inferred to be case-sensitive...
@@ -37,7 +50,6 @@ attr(vocab_cased, "is_cased")
 wordpiece_tokenize(text = "And I love tacos and salsa!", vocab = vocab_cased)
 
 ## ----example3-----------------------------------------------------------------
-
 wordpiece_tokenize(text = "I love tacos!", 
                    vocab = vocab_cased, 
                    unk_token = "[missing]")
